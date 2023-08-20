fc550bb706508b1c8663d0b490fe3450ba08e770
test person <test@test.net>
2023-08-20 14:16:34 +0530

    Implement sort and select product fields

--- controllers/products.js
+++ controllers/products.js
@@ -3,14 +3,12 @@ const Product = require('../models/product')
 const getAllProductsStatic = async (req, res) => {
     // throw new Error('Testing async error')
     const search = 'secti'
-    const products = await Product.find({
-        name: {$regex:search, $options: 'i'},
-    })
+    const products = await Product.find({}).select('name price')
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name } = req.query
+    const { featured, company, name, sort, fields } = req.query
     const queryObject = {}
 
     if(name){
@@ -23,8 +21,22 @@ const getAllProducts = async (req, res) => {
     if(company){
         queryObject.company = company
     }
-    console.log(queryObject);
-    const products = await Product.find(queryObject)
+    // console.log(queryObject);
+    // const products = await Product.find(queryObject)
+    let result = Product.find(queryObject)
+    // sort
+    if(sort){
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('createdAt')
+    }
+    // select 
+    if(fields){
+        const fieldsList = fields.split(',').join(' ') 
+        result = result.select(fieldsList)
+    }
+    const products = await result
     res.status(200).json({ products, nbHits: products.length })
 }
 
