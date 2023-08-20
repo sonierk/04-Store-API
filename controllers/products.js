e14af1ae30eafb1bf926fde85e59e5f474dbe157
test person <test@test.net>
2023-08-20 14:32:40 +0530

    Implement Pagination

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,10 @@ const Product = require('../models/product')
 const getAllProductsStatic = async (req, res) => {
     // throw new Error('Testing async error')
     const search = 'secti'
-    const products = await Product.find({}).select('name price')
+    const products = await Product.find({})
+        .sort('name')
+        .select('name price')
+        .limit(10)
     res.status(200).json({ products, nbHits: products.length })
 }
 
@@ -36,6 +39,13 @@ const getAllProducts = async (req, res) => {
         const fieldsList = fields.split(',').join(' ') 
         result = result.select(fieldsList)
     }
+    // pagination 
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+    result = result.skip(skip).limit(limit)
+
+
     const products = await result
     res.status(200).json({ products, nbHits: products.length })
 }
